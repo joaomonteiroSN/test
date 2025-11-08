51b1847791d0b9a1057c5a25427643ec88ba32a5
joaoMonteiro <70484799+joaomonteiroSN@users.noreply.github.com>
2025-11-07 22:54:22 -0300

    adiação na branch 3

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 const headerEl = document.getElementsByTagName('h1');
 console.log(headerEl);
+
+// fingindo que estou fazendo alguams atualizações no projeto \
+
+() => {
+    console.log('okokok')
+}
